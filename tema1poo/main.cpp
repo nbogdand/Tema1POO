451d537137d94c567b9dead1cb6d5f5fc82cec77
Bogdan Noana <nbogdand@gmail.com>
2018-10-20 22:58:40 +0300

    am adaugat atributele,setteri,getteri,constructori

--- tema1poo/main.cpp
+++ tema1poo/main.cpp
@@ -1,9 +1,68 @@
 #include <iostream>
 
+
+class Numar_rational{
+private:
+    int numarator;
+    int numitor;
+public:
+
+    Numar_rational(int numarator=0,int numitor=1){
+        this->numarator=numarator;
+        this->numitor=numitor;
+    }
+
+    int getNumarator(){
+        return this->numarator;
+    }
+    void setNumarator(int numarator){
+        this->numarator=numarator;
+    }
+
+    int getNumitor(){
+        return this->numitor;
+    }
+
+    void setNumitor(int numitor){
+        this->numitor=numitor;
+    }
+
+    Numar_rational operator + (Numar_rational& r){
+        Numar_rational nr;
+        nr.numarator=this->numarator*r.numitor + r.numarator*this->numitor;
+        nr.numitor=this->numitor*r.numitor;
+        simplifica(nr);
+        return nr;
+    }
+
+private:
+    int cmmdc(int a,int b){
+        while(a!=b){
+            if(a>b)
+                a-=b;
+            else
+                b-=a;
+        }
+        return a;
+    }
+
+    void simplifica(Numar_rational& r){
+        int cmmdcF=cmmdc(r.numarator,r.numitor);
+        r.numarator=r.numarator/cmmdcF;
+        r.numitor=r.numitor/cmmdcF;
+    }
+
+};
+
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Numar_rational r1(2,3);
+    Numar_rational r2(4,5);
+    Numar_rational aux;
+    aux=r1+r2;
+    cout<<aux.getNumarator()<<"/"<<aux.getNumitor();
     return 0;
 }
